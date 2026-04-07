1cd71d40f6aff4ffd3ef8146a2d4af8ebdc85f71
brunoloo <brunodepaula.trabajo@gmail.com>
2026-04-07 00:11:46 -0300

    updates/dev

--- project/backend/src/app.js
+++ project/backend/src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 // Import routes
 import API from './routes/API.js';
 import index from './routes/index.js';
 
 const app = express();
+
+// Cookie
+app.use(cookieParser());
 
 // Middleware
 app.use(express.json());
