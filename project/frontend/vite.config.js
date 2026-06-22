7e0896c251299a6a7e495119051f9dbbe4654331
brunoloo <brunodepaula.trabajo@gmail.com>
2026-06-22 18:55:51 -0300

    allowed ngrok

--- project/frontend/vite.config.js
+++ project/frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: ['carload-chivalry-sinuous.ngrok-free.dev'],
     proxy: {
       '/api': 'http://localhost:5001',
       '/central': 'http://localhost:5001',
