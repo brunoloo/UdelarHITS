5114f491ccae0c228321eb566017cfb93a1c408e
brunoloo <brunodepaula.trabajo@gmail.com>
2026-06-27 22:30:05 -0300

    fix

--- project/backend/src/socket.js
+++ project/backend/src/socket.js
@@ -1,5 +1,5 @@
 import { Server } from 'socket.io';
-import { parse as parseCookie } from 'cookie';
+import * as cookieLib from 'cookie';
 import jwt from 'jsonwebtoken';
 import pool from './config/db.js';
 
