3ce3ca2cf50fdc15fa68009916db2a51097c168f
brunoloo <brunodepaula.trabajo@gmail.com>
2026-07-09 23:13:31 -0300

    fix

--- project/frontend/vite.config.js
+++ project/frontend/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+function stripCrossorigin() {
+  return {
+    name: 'strip-crossorigin',
+    transformIndexHtml(html) {
+      return html.replace(/\s+crossorigin/g, '')
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), stripCrossorigin()],
   server: {
     port: 5173,
     allowedHosts: ['carload-chivalry-sinuous.ngrok-free.dev'],
